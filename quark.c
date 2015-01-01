0974d9ef4a4bebb9a410f4a41237cafcff96a91b
BinRui Guan <binrui.guan@gmail.com>
2015-01-01 12:02:36 +0800

    Parse number implementation

--- quark.c
+++ quark.c
@@ -1,79 +1,112 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include "quark.h"
 
 #define EXPECT(c, ch)\
     do {\
         assert(*c->json == (ch)); c->json++;\
     } while(0)
+#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
+#define IS_E(c) ((c) == 'e' || (c) == 'E')
+#define IS_NOT_E(c) ((c) != 'e' && (c) != 'E')
 
 typedef struct {
     const char *json;
 } quark_context;
 
-static int quark_parse_true(quark_context *context, quark_node *node) {
-    EXPECT(context, 't');
+static int quark_parse_literal(quark_context *context, quark_node *node, const char *literal, quark_type type) {
+    int len = 0;
+    const char *json = context->json;
 
-    if (
-        context->json[0] != 'r' ||
-        context->json[1] != 'u' ||
-        context->json[2] != 'e') {
-        return QUARK_PARSE_INVALID_VALUE;
+    while (*literal != '\0') {
+        if (*json == *literal) {
+            len++;
+            json++;
+            literal++;
+        } else {
+            return QUARK_PARSE_INVALID_VALUE;
+        }
     }
 
-    context->json += 3;
-    node->type = QUARK_TRUE;
-
+    context->json += len;
+    node->type = type;
     return QUARK_PARSE_OK;
 }
 
-static int quark_parse_false(quark_context *context, quark_node *node) {
-    EXPECT(context, 'f');
+static int quark_parse_number(quark_context *context, quark_node *node) {
+    char *end;
+    char c = '\0';
+    char last_c = '\0';
+    int in_farc = 0;
+    int in_exp = 0;
+    const char *json = context->json;
 
-    if (
-        context->json[0] != 'a' ||
-        context->json[1] != 'l' ||
-        context->json[2] != 's' ||
-        context->json[3] != 'e') {
-        return QUARK_PARSE_INVALID_VALUE;
+    while ((c = *json) != '\0' && c != ' ' && c != '\t' && c != '\n' && c != '\r') {
+        if (IS_DIGIT(c)) {
+            if (c == '0' && (last_c == '\0' || last_c == '-') && IS_DIGIT(*(json + 1)))
+                return QUARK_PARSE_INVALID_VALUE;
+
+        } else {
+            switch (c) {
+                case '-':
+                    if (last_c != '\0' && IS_NOT_E(last_c))
+                        return QUARK_PARSE_INVALID_VALUE;
+                    break;
+
+                case '+':
+                    if (IS_NOT_E(last_c))
+                        return QUARK_PARSE_INVALID_VALUE;
+                    break;
+
+                case 'e':
+                case 'E':
+                    if (in_exp || last_c == '\0')
+                        return QUARK_PARSE_INVALID_VALUE;
+                    else
+                        in_exp = 1;
+                    break;
+
+                case '.':
+                    if (in_farc || last_c == '\0' || !IS_DIGIT(*(json + 1)))
+                        return QUARK_PARSE_INVALID_VALUE;
+                    else
+                        in_farc = 1;
+                    break;
+
+                default:
+                    return QUARK_PARSE_INVALID_VALUE;
+            }
+        }
+
+        json++;
+        last_c = c;
     }
 
-    context->json += 4;
-    node->type = QUARK_FALSE;
-
-    return QUARK_PARSE_OK;
-}
-
-static int quark_parse_null(quark_context *context, quark_node *node) {
-    EXPECT(context, 'n');
-
-    if (context->json[0] != 'u' || context->json[1] != 'l' || context->json[2] != 'l') {
+    node->number = strtod(context->json, &end);
+    if (context->json == end)
         return QUARK_PARSE_INVALID_VALUE;
-    }
-
-    context->json += 3;
-    node->type = QUARK_NULL;
-
+    context->json = end;
+    node->type = QUARK_NUMBER;
     return QUARK_PARSE_OK;
 }
 
 static void quark_parse_whitespace(quark_context *context) {
     const char *p = context->json;
 
-    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r') {
+    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
         p++;
-    }
 
     context->json = p;
 }
 
 static int quark_parse_value(quark_context *context, quark_node *node) {
     switch (*context->json) {
-        case 'n':  return quark_parse_null(context, node);
-        case 't':  return quark_parse_true(context, node);
-        case 'f':  return quark_parse_false(context, node);
         case '\0': return QUARK_PARSE_EXPECT_VALUE;
-        default :  return QUARK_PARSE_INVALID_VALUE;
+        case 'n':  return quark_parse_literal(context, node, "null", QUARK_NULL);
+        case 't':  return quark_parse_literal(context, node, "true", QUARK_TRUE);
+        case 'f':  return quark_parse_literal(context, node, "false", QUARK_FALSE);
+        default :  return quark_parse_number(context, node);
     }
 }
 
@@ -89,11 +122,18 @@ int quark_parse(quark_node *node, const char *json) {
         quark_parse_whitespace(&context);
 
         if (*context.json != '\0') {
+            node->type = QUARK_NULL;
             rtn = QUARK_PARSE_ROOT_NODE_SINGULAR;
         }
     }
 
     return rtn;
+}
+
+double quark_get_number(const quark_node *node) {
+    assert(node != NULL && node->type == QUARK_NUMBER);
+
+    return node->number;
 }
 
 quark_type quark_get_type(const quark_node *node) {
--- quark.h
+++ quark.h
@@ -12,6 +12,7 @@ typedef enum {
 } quark_type;
 
 typedef struct {
+    double number;
     quark_type type;
 } quark_node;
 
@@ -19,9 +20,11 @@ enum quark_result {
     QUARK_PARSE_OK,
     QUARK_PARSE_EXPECT_VALUE,
     QUARK_PARSE_INVALID_VALUE,
-    QUARK_PARSE_ROOT_NODE_SINGULAR
+    QUARK_PARSE_ROOT_NODE_SINGULAR,
+    QUARK_PARSE_NUMBER_TOO_BIG
 };
 
 int quark_parse(quark_node *, const char *);
+double quark_get_number(const quark_node *);
 quark_type quark_get_type(const quark_node *);
 #endif
